669ac54e54b7cef267fda038f5c9fd234aee3a7e
Max Vu <maxwell.vu@gmail.com>
2017-10-20 18:54:58 -0400

    Add HandOption

---
+++ include/HandOption.hpp
@@ -0,0 +1,16 @@
+#ifndef BJSIM4_HANDOPTION
+#define BJSIM4_HANDOPTION
+
+namespace bjsim4 {
+
+    enum HandOption {
+        STAND,
+        HIT,
+        DOUBLEDOWN,
+        SPLIT,
+        SURRENDER
+    };
+
+};
+
+#endif
